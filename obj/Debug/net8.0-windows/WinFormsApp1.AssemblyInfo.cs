aa98ab6bdba73e5aebdd14d13ff13db638356bcd
Cucumberi135 <action.ytert@gmail.com>
2025-10-06 09:44:57 +0300

    ble

--- obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc65d38092659aa3467643f94b396b4efba66701")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de88250cf89b566e668f6ffcf80a534f22d07178")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
